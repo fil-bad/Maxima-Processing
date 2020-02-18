0b90d56fd2ac91f3516b829e097545fcac45e3b0
fil-bad <filibad25051997@gmail.com>
2020-02-18 16:33:16 +0100

    Added Separating Axis Theorem class

---
+++ Processing/Progetti Esame/For_IntelliJ/src/javaMisc/Vertex.java
@@ -0,0 +1,39 @@
+package javaMisc;
+
+public class Vertex {
+        private double x, y;
+
+        Vertex(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        /**
+         * Returns a new vector which is orthogonal to the current vector
+         */
+        public Vertex orthogonal() {
+            return new Vertex(y, -x);
+        }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void printVertex() {
+            System.out.print("[" + this.x + ";" + this.y + "]");
+        }
+
+        public static void main(String[] args){
+            Vertex v1 = new Vertex(50, 12);
+            v1.printVertex();
+            Vertex v2 = v1.orthogonal();
+            System.out.println("");
+            System.out.print("orthogonal of v1:");
+            v2.printVertex();
+        }
+}
+
