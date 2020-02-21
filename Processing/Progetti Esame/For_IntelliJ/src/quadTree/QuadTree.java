ac59022172b5644d7920fbde0732b78f31762c11
alfystar <alfystar1701@gmail.com>
2020-02-21 20:14:58 +0100

    Quadtree debug print emprove

--- Processing/Progetti Esame/For_IntelliJ/src/quadTree/QuadTree.java
+++ Processing/Progetti Esame/For_IntelliJ/src/quadTree/QuadTree.java
@@ -1,6 +1,6 @@
 package quadTree;
-import static quadTree.Coord.NW;
-import static quadTree.Coord.SE;
+
+import static quadTree.Coord.*;
 
 /*
  *  			N
@@ -10,14 +10,14 @@ import static quadTree.Coord.SE;
 
 /**
  * Ogni classe QuadTree è essa stessa un nodo, se
- *     QuadTree northWest = null;
- *     QuadTree northEast = null;
- *     QuadTree southWest = null;
- *     QuadTree southEast = null;
+ * QuadTree northWest = null;
+ * QuadTree northEast = null;
+ * QuadTree southWest = null;
+ * QuadTree southEast = null;
  * Allora è una foglia, altrimenti è uno splitPoint
- * */
+ */
 public class QuadTree {
-    private final int MAX_CAPACITY =4;
+    private final int MAX_CAPACITY = 4;
     private int level = 0;
 
     private boolean freeSpace;
@@ -34,14 +34,14 @@ public class QuadTree {
         freeSpace = true;
     }
 
-    public QuadTree getNode(Coord c){
-        if(c==Coord.NE)
+    public QuadTree getNode(Coord c) {
+        if (c == Coord.NE)
             return northEast;
-        if(c==Coord.NW)
+        if (c == Coord.NW)
             return northWest;
-        if(c==Coord.SW)
+        if (c == Coord.SW)
             return southWest;
-        if(c== SE)
+        if (c == SE)
             return southEast;
         return null;
     }
@@ -51,27 +51,46 @@ public class QuadTree {
         if (node == null)
             return;
 
-        System.out.printf("\nLevel = %d [X1=%2f Y1=%2f] \t[X2=%2f Y2=%2f] ",
+        System.out.printf("L%d [X1=%.2f Y1=%.2f] \t[X2=%.2f Y2=%.2f]\n",
                 node.level, node.boundry.getxMin(), node.boundry.getyMin(),
                 node.boundry.getxMax(), node.boundry.getyMax());
 
         if (node.isLeaf()) {
-            System.out.printf(" \n\t  Leaf Node. FreeSpace:"+node.isFreeSpace());
+            System.out.print("|");
+            for (int i = 0; i < node.level; i++)
+                System.out.print("\t");
+            System.out.println("\tLeaf Node. FreeSpace:" + node.isFreeSpace());
+        } else {
+            for (int i = 0; i < node.level; i++)
+                System.out.print("|-----");
+            System.out.print("NE:");
+            dfs(node.northEast);
+
+            for (int i = 0; i < node.level; i++)
+                System.out.print("|-----");
+            System.out.print("NW:");
+            dfs(node.northWest);
+
+            for (int i = 0; i < node.level; i++)
+                System.out.print("|-----");
+            System.out.print("SW:");
+            dfs(node.southWest);
+
+            for (int i = 0; i < node.level; i++)
+                System.out.print("|-----");
+            System.out.print("SE:");
+            dfs(node.southEast);
         }
-        dfs(node.northEast);
-        dfs(node.northWest);
-        dfs(node.southWest);
-        dfs(node.southEast);
 
     }
 
-    public boolean isLeaf(){
+    public boolean isLeaf() {
         return northWest == null && northEast == null &&
                 southWest == null && southEast == null;
     }
 
     public void split() throws RuntimeException {
-        if(!isLeaf())
+        if (!isLeaf())
             throw new RuntimeException("Non è una foglia");
 
         double xOffset = boundry.getX();
@@ -108,6 +127,7 @@ public class QuadTree {
         qt.split();
         qt.getNode(SE).split();
         qt.getNode(SE).getNode(NW).setFreeSpace(false);
+        qt.getNode(SE).getNode(NW).split();
         //Traveling the graph
         QuadTree.dfs(qt);
     }
