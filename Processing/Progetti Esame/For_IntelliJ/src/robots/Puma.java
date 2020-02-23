346a8a09cc71da9548797f0eb1d21eebf9ce0392
fil-bad <filibad25051997@gmail.com>
2020-02-23 17:18:59 +0100

    [Start] Puma & robot design

--- Processing/Progetti Esame/For_IntelliJ/src/robots/Puma.java
+++ Processing/Progetti Esame/For_IntelliJ/src/robots/Puma.java
@@ -1,21 +1,27 @@
 package robots;
 
+import com.jogamp.opengl.math.Matrix4;
+import org.ejml.data.Matrix;
 import processingElement.DenHart;
 
 public class Puma {
 
     private DenHart dhTab;
+    private Matrix Q;
 
     public Puma(DenHart denHart) {
         this.dhTab = denHart;
+
+    }
+
+
+    public void printDHTab() {
+        this.dhTab.printDHTab();
     }
 
     public static void main(String[] args) {
         Puma puma = new Puma(new DenHart());
     }
 
-    public DenHart getDhTab() {
-        return dhTab;
-    }
 
 }
