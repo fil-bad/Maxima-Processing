abc53a48b044b8784d08c11b6377b7d814df3a00
fil-bad <filibad25051997@gmail.com>
2020-02-26 12:23:45 +0100

    [START] AutoDiff & DenHart

--- Processing/Progetti Esame/For_IntelliJ/src/javaMisc/Link.java
+++ Processing/Progetti Esame/For_IntelliJ/src/javaMisc/Link.java
@@ -10,4 +10,5 @@ public interface Link {
 
     void update();
 
+
 }
---
+++ Processing/Progetti Esame/For_IntelliJ/src/javaMisc/MatrixQ.java
@@ -0,0 +1,14 @@
+package javaMisc;
+
+public class MatrixQ {
+
+    // 4x4 dimension, as the below one
+    //  ________________
+    // |       .        |
+    // |  R(q) . R(q)*d |
+    // |................|
+    // | 0 0 0 .   1    |
+    // |________________|
+
+
+}
